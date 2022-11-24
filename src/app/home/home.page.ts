b42eedb8d29e8c7dc833d2fff6d6d33fff197063
Christoph <christph.burscher@bbv.ch>
2022-11-24 11:58:51 +0100

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public buffer = 0.06;
+  public progress = 0;
+  imageUrl: string = '';
+  pluginResult = '';
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async takePhoto() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+      });
+
+      if (image.webPath) {
+        this.imageUrl = image.webPath;
+      }
+    } catch (exception) {
+      console.error(exception);
+    }
+  }
+
+  startDownload() {
+    setInterval(() => {
+      this.buffer += 0.06;
+      this.progress += 0.06;
+
+      // Reset the progress bar when it reaches 100%
+      // to continuously show the demo
+      if (this.progress > 1) {
+        setTimeout(() => {
+          this.buffer = 0.06;
+          this.progress = 0;
+        }, 1000);
+      }
+    }, 1000);
+  }
 }
